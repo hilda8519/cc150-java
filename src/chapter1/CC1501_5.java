dd556724c4ba386f004eb3cd7ac95ee0c05a41ef
hilda8519 <hilda8519@gmail.com>
2015-03-26 22:42:55 -0700

    CC150 1-5

---
+++ src/chapter1/CC1501_5.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package chapter1;
+
+/**
+ * @author MaoTouMaoTou
+ *
+ */
+public class CC1501_5 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
